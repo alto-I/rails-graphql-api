29a554b50965fc0970752c47ad54716671c56dfe
ito_arata <blmaldiy@gmail.com>
2022-11-16 15:29:54 +0900

    不要な文字列を削除

--- app/graphql/mutations/update_post.rb
+++ app/graphql/mutations/update_post.rb
@@ -3,7 +3,6 @@ module Mutations
     argument :params, Types::Inputs::PostInputType, required: true
 
     def resolve(params:)
-      p "⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎実行できてる？⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎⭐︎"
       post_params = params.to_h
       post = Post.find(post_params.delete(:id))
       post.update!(post_params.compact)
